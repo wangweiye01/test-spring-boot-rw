0a27241c41ab4ef9f107ce5f9d37511bee6fc56d
wwy <wwyknight@163.com>
2017-04-24 17:39:24 +0800

    fix

--- src/main/java/mytestx/config/DataSourceType.java
+++ src/main/java/mytestx/config/DataSourceType.java
@@ -15,8 +15,21 @@ public enum DataSourceType {
         this.name = name;
     }
 
+	public String getType() {
+		return type;
+	}
 
-    public Object getType() {
-        return type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
 }
